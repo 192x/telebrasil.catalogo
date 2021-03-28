08483125b5bab5b3108287002d96eefc0d14364a
Alex Souza <59103542+192x@users.noreply.github.com>
2021-03-28 01:03:32 -0300

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,146 +1,620 @@
 var itensInfo = [
-    `<ul id="cxitem">
-    <h2>Suporte para Notebook - 0100</h2>
+    `<ul class="cxitem">
+    <h2 class='h2Tit'>CAIXAOOPSON <span id="codSp">3563</span></h2>
     <figure>
-        <img src="0100.png">
+        <img src="3563.jpg">
         <figcaption>
-            <button>Saber mais...</button>
+            <button onclick="avPrd()">Saber mais...</button>
         </figcaption>
     </figure>
+    </ul>`,
+//tr ----------------------    
+    `<ul class="cxitem">
+    <h2 class='h2Tit'>MICROFONE MIC-001 HOOPSON  <span id="codSp">3372</span></h2>
+    <figure>
+        <img src="3372.jpg">
+        <figcaption>
+        <button onclick="avPrd()">Saber mais...</button>
+        </figcaption>
+    </figure>
+    </ul>`,
+//tr ----------------------    
+    `<ul class="cxitem">
+    <h2 class='h2Tit'>CAIXASUMAY <span id="codSp">4059</span></h2>
+    <figure>
+        <img src="4059.jpg">
+        <figcaption>
+        <button onclick="avPrd()">Saber mais...</button>
+        </figcaption>
+    </figure>
+    </ul>`,
+//tr ----------------------    
+    `<ul class="cxitem">
+    <h2 class='h2Tit'>CAIXA SUMAY <span id="codSp">4055</span></h2>
+    <figure>
+        <img src="4055.jpg">
+        <figcaption>
+        <button onclick="avPrd()">Saber mais...</button>
+        </figcaption>
+    </figure>
+    </ul>`,
+//tr ----------------------    
+    `<ul class="cxitem">
+    <h2 class='h2Tit'>CAIXA SUMAY <span id="codSp">4057</span></h2>
+    <figure>
+        <img src="4057.jpg">
+        <figcaption>
+        <button onclick="avPrd()">Saber mais...</button>
+        </figcaption>
+    </figure>
+    </ul>`,
+//tr ----------------------    
+    `<ul class="cxitem">
+    <h2 class='h2Tit'>CAIXA SUMAY <span id="codSp">4058</span></h2>
+    <figure>
+        <img src="4058.jpg">
+        <figcaption>
+        <button onclick="avPrd()">Saber mais...</button>
+        </figcaption>
+    </figure>
+    </ul>`,
+//tr ----------------------    
+    `<ul class="cxitem">
+    <h2 class='h2Tit'>qqCAIXA DE SOM SK405 OEX <span id="codSp">2412</span></h2>
+    <figure>
+        <img src="2412.jpg">
+        <figcaption>
+        <button onclick="avPrd()">Saber mais...</button>
+        </figcaption>
+    </figure>
+    </ul>`,
+    //tr ----------------------    
+        `<ul class="cxitem">
+        <h2 class='h2Tit'>VITROLA BERRY RETRÔ SP364 PULSE <span id="codSp">4164</span></h2>
+        <figure>
+            <img src="4164.jpg">
+            <figcaption>
+            <button onclick="avPrd()">Saber mais...</button>
+            </figcaption>
+        </figure>
+        </ul>`,
+    
+//tr ----------------------    
+    `<ul class="cxitem">
+    <h2 class='h2Tit'>VITROLA PERKINS RETRÔ SP365 PULSE <span id="codSp">4165</span></h2>
+    <figure>
+        <img src="4165.jpg">
+        <figcaption>
+        <button onclick="avPrd()">Saber mais...</button>
+        </figcaption>
+    </figure>
+    </ul>`,
+    
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>COOLER PARA NOTEBOOK HOOPSON <span id="codSp">3611</span></h2>
+<figure>
+    <img src="3611.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
 </ul>`,
-    `<ul id="cxitem">
-    <h2>Suporte para Notebook - 0100</h2>
-    <figure>
-        <img src="0100.png">
-        <figcaption>
-            <button>Saber mais...</button>
-        </figcaption>
-    </figure>
+
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>COOLER PARA NOTEBOOK HOOPSON <span id="codSp">4214</span></h2>
+<figure>
+    <img src="4214.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
 </ul>`,
-    `<ul id="cxitem">
-    <h2>Suporte para Notebook - 0100</h2>
-    <figure>
-        <img src="0100.png">
-        <figcaption>
-            <button>Saber mais...</button>
-        </figcaption>
-    </figure>
+
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>CONTROLE VIDEOGAME PS3 XLS <span id="codSp">3965</span></h2>
+<figure>
+    <img src="3965.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
 </ul>`,
-    `<ul id="cxitem">
-    <h2>Suporte para Notebook - 0100</h2>
-    <figure>
-        <img src="0100.png">
-        <figcaption>
-            <button>Saber mais...</button>
-        </figcaption>
-    </figure>
+
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>CONTROLE VIDEOGAME PS4 XLS <span id="codSp">4211</span></h2>
+<figure>
+    <img src="4211.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
 </ul>`,
-    `<ul id="cxitem">
-    <h2>Suporte para Notebook - 0100</h2>
-    <figure>
-        <img src="0100.png">
-        <figcaption>
-            <button>Saber mais...</button>
-        </figcaption>
-    </figure>
+
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>REPETIDOE DE SINAL WI-FI TPLINK <span id="codSp">1381</span></h2>
+<figure>
+    <img src="1381.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
 </ul>`,
-    `<ul id="cxitem">
-    <h2>Suporte para Notebook - 0100</h2>
-    <figure>
-        <img src="0100.png">
-        <figcaption>
-            <button>Saber mais...</button>
-        </figcaption>
-    </figure>
+
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>ROTEADOR WIFI 150MBPS MULTILASER <span id="codSp">2022</span></h2>
+<figure>
+    <img src="2022.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>ROTEADOR WIFI 2/5G 300/867MBPS TPLINK <span id="codSp">3640</span></h2>
+<figure>
+    <img src="3640.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>TV BOX 4K ANDROID 10.1 <span id="codSp">3986</span></h2>
+<figure>
+    <img src="3986.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>TELEFONE RURAL AQUÁRIO DUAL CHIP <span id="codSp">2487</span></h2>
+<figure>
+    <img src="2487.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>TELEFONE FIXO C/FIO INTELBRAS <span id="codSp">4086</span></h2>
+<figure>
+    <img src="4086.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>RAMAL SEM FIO DIGITAL INTELBRAS <span id="codSp">3915</span></h2>
+<figure>
+    <img src="3915.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>TELEFONE SEM FIO PANASONIC 2X1 <span id="codSp">3456</span></h2>
+<figure>
+    <img src="3456.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>GAVETA HD/GRAVADORA CD NOTEBOOK <span id="codSp">3555</span></h2>
+<figure>
+    <img src="3555.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>CASE 3.0 PARA HD NOTEBOOK KNUP <span id="codSp">4223</span></h2>
+<figure>
+    <img src="4223.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>CONVERSOR HDMI PARA VGA <span id="codSp">3635</span></h2>
+<figure>
+    <img src="3635.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>ADAPTADOR BLUETOOTH P2 <span id="codSp">3503</span></h2>
+<figure>
+    <img src="3503.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>CASE 2.0 PARA HD NOTEBOOK HOOPSON <span id="codSp">4018</span></h2>
+<figure>
+    <img src="4018.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>KIT SMARTPHONE MULTILASER <span id="codSp">3852</span></h2>
+<figure>
+    <img src="3852.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>SWITCH HDMI 3 PORTAS MULTILASER <span id="codSp">2897</span></h2>
+<figure>
+    <img src="2897.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>HUB USB SLIM 2.0 4 PORTAS <span id="codSp">4153</span></h2>
+<figure>
+    <img src="4153.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>CONVERSOR USB 3.0 PARA HDMI <span id="codSp">3473</span></h2>
+<figure>
+    <img src="3473.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>SWITCH HDMI 3 PORTAS MULTILASER <span id="codSp">2897</span></h2>
+<figure>
+    <img src="2897.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>ADAPTADOR USB WIRELESS PC <span id="codSp">3092</span></h2>
+<figure>
+    <img src="3092.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>APRESENTADOR SLIDE SEM FIO C/LASER MULTILASER <span id="codSp">2643</span></h2>
+<figure>
+    <img src="2643.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>MOUSE PAD GAMER 30/70CM WARRIOR <span id="codSp">4156</span></h2>
+<figure>
+    <img src="4156.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>MOUSE PAD GAMER 25/34CM WARRIOR <span id="codSp">4155</span></h2>
+<figure>
+    <img src="4155.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>MOUSE PAD 18/22CM KNUP <span id="codSp">3908</span></h2>
+<figure>
+    <img src="3908.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>MICROFONE MESA PROFISSIONAL XTRAD <span id="codSp">4045</span></h2>
+<figure>
+    <img src="4045.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>MOUSE PAD HOOPSON C/APOIO PULSO <span id="codSp">4218</span></h2>
+<figure>
+    <img src="4218.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>CABO HDMI 5M HOOPSON <span id="codSp">2881</span></h2>
+<figure>
+    <img src="2881.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>CABO HDMI 2M HOOPSON <span id="codSp">2880</span></h2>
+<figure>
+    <img src="2880.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>MOUSE GAMER HOOPSON MS-30 <span id="codSp">2819</span></h2>
+<figure>
+    <img src="2819.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>MOUSE GAMER HOOPSON GXW-900 <span id="codSp">4216</span></h2>
+<figure>
+    <img src="4216.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>MOUSE GAMER MULTILASER MO208 <span id="codSp">3589</span></h2>
+<figure>
+    <img src="3589.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>MOUSE GAMER HOOPSON GT1000 <span id="codSp">4215</span></h2>
+<figure>
+    <img src="4215.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>MOUSE HOOPSON C/FIO MS-035P <span id="codSp">2871</span></h2>
+<figure>
+    <img src="2871.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>MOUSE HOOPSNO C/FIO MS-032 <span id="codSp">2870</span></h2>
+<figure>
+    <img src="2870.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>MOUSE HOOPSON C/FIO MS-035R <span id="codSp">4084</span></h2>
+<figure>
+    <img src="4084.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>MOUSE MICROSOFT S/FIO 1850 <span id="codSp">2855</span></h2>
+<figure>
+    <img src="2855.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>MOUSE MICROSOFT C/FIO 4500 <span id="codSp">3624</span></h2>
+<figure>
+    <img src="3624.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
+</ul>`,
+//tr ----------------------    
+`<ul class="cxitem">
+<h2 class='h2Tit'>MOUSE PHILIPS C/FIO M302 <span id="codSp">3972</span></h2>
+<figure>
+    <img src="3972.jpg">
+    <figcaption>
+    <button onclick="avPrd()">Saber mais...</button>
+    </figcaption>
+</figure>
 </ul>`,
 ]
 itensInfo.sort()
 
 var itensCell = [
-    `<ul id="cxitem">
-    <h2>Cabo nylon i2GO 2m - 0101</h2>
+    `<ul class="cxitem">
+    <h2>Cabo nylon i2GO 2m - 4215</h2>
     <figure>
-        <img src="0101.png">
+        <img src="4215.jpg">
         <figcaption>
-            <button>Saber mais...</button>
+            <button onclick="avPrd()">Saber mais...</button>
         </figcaption>
     </figure>
 </ul>`,
-    `<ul id="cxitem">
-    <h2>Cabo nylon i2GO 2m - 0101</h2>
+//tr ----------------------
+
+    `<ul class="cxitem">
+    <h2>Cabo nylon i2GO 2m - 4215</h2>
     <figure>
-        <img src="0101.png">
+        <img src="4215.jpg">
         <figcaption>
-            <button>Saber mais...</button>
+            <button onclick="avPrd()">Saber mais...</button>
         </figcaption>
     </figure>
 </ul>`,
-    `<ul id="cxitem">
-    <h2>Cabo nylon i2GO 2m - 0101</h2>
+//tr ----------------------
+
+    `<ul class="cxitem">
+    <h2>Cabo nylon i2GO 2m - 4215</h2>
     <figure>
-        <img src="0101.png">
+        <img src="4215.jpg">
         <figcaption>
-            <button>Saber mais...</button>
+            <button onclick="avPrd()">Saber mais...</button>
         </figcaption>
     </figure>
 </ul>`,
-    `<ul id="cxitem">
-    <h2>Cabo nylon i2GO 2m - 0101</h2>
+//tr ----------------------
+
+    `<ul class="cxitem">
+    <h2>Cabo nylon i2GO 2m - 4215</h2>
     <figure>
-        <img src="0101.png">
+        <img src="4215.jpg">
         <figcaption>
-            <button>Saber mais...</button>
+            <button onclick="avPrd()">Saber mais...</button>
         </figcaption>
     </figure>
 </ul>`,
-    `<ul id="cxitem">
-    <h2>Cabo nylon i2GO 2m - 0101</h2>
+//tr ----------------------
+
+    `<ul class="cxitem">
+    <h2>Cabo nylon i2GO 2m - 4215</h2>
     <figure>
-        <img src="0101.png">
+        <img src="4215.jpg">
         <figcaption>
-            <button>Saber mais...</button>
+            <button onclick="avPrd()">Saber mais...</button>
         </figcaption>
     </figure>
 </ul>`,
-    `<ul id="cxitem">
-    <h2>Cabo nylon i2GO 2m - 0101</h2>
+//tr ----------------------
+
+    `<ul class="cxitem">
+    <h2>Cabo nylon i2GO 2m - 4215</h2>
     <figure>
-        <img src="0101.png">
+        <img src="4215.jpg">
         <figcaption>
-            <button>Saber mais...</button>
+            <button onclick="avPrd()">Saber mais...</button>
         </figcaption>
     </figure>
 </ul>`,
+//tr ----------------------
+
 ]
 itensCell.sort()
 
 setTimeout(loadItens,0)
 
 var itensJob = [
-    `<ul id="cxitem">
+    `<ul class="cxitem">
     <h2>Visita Técnica - 0100</h2>
     <figure>
         <figcaption>
-            <button>Saber mais...</button>
+            <button onclick="avPrd()">Saber mais...</button>
         </figcaption>
     </figure>
 </ul>`,
-    `<ul id="cxitem">
+//tr ----------------------
+
+    `<ul class="cxitem">
     <h2>Formatação sem Backup - 0102</h2>
     <figure>
         <figcaption>
-            <button>Saber mais...</button>
+            <button onclick="avPrd()">Saber mais...</button>
         </figcaption>
     </figure>
 </ul>`,
-    `<ul id="cxitem">
+//tr ----------------------
+
+    `<ul class="cxitem">
     <h2>Formatação com Backup - 0100</h2>
     <figure>
         <figcaption>
-            <button>Saber mais...</button>
+            <button onclick="avPrd()">Saber mais...</button>
         </figcaption>
     </figure>
 </ul>`,
+//tr ----------------------
+
 ]
 itensJob.sort()
 function loadItens(){
@@ -150,6 +624,7 @@ function loadItens(){
     cell.innerHTML+=itensCell
     var job = document.getElementById("cxJob")
     job.innerHTML+=itensJob
-    
-    
+}
+function avPrd(){
+    alert('Ainda em desenvolvimento. :)')
 }
